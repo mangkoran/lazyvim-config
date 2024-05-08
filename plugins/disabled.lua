c2dd4b8ae40f551e26c292765c8ad062300e785f
Afiq Nazrie <afnazrie@gmail.com>
2024-05-08 08:09:55 +0700

    fix: don't lazy-load dashboard

--- plugins/disabled.lua
+++ plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   -- { "echasnovski/mini.indentscope", enabled = false },
   -- { "stevearc/conform.nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
 }
---
+++ plugins/zz-sandbox.lua
@@ -0,0 +1,28 @@
+-- Test plugin, config etc. here
+
+-- vim.api.nvim_create_augroup("dashboard_stdin", { clear = true })
+-- vim.api.nvim_create_autocmd({ "StdinReadPre" }, {
+--   group = "dashboard_stdin",
+--   callback = function()
+--     vim.g.loaded_dashboard = 1
+--   end,
+-- })
+
+-- vim.api.nvim_create_augroup("dashboard", { clear = true })
+-- vim.api.nvim_create_autocmd("StdinReadPre", {
+--   group = "dashboard",
+--   callback = function()
+--     vim.g.read_from_stdin = 1
+--   end,
+-- })
+
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    -- dir = "~/repos/fork/dashboard-nvim",
+    lazy = false,
+    -- priority = 1000,
+    -- commit = "354e7bd0ad62f6e8b5ab9394b1ec7dbcaaa1470c",
+    -- enabled = false
+  },
+}
